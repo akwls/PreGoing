7075a2092d42ae2218fa4b4ca3f39d1bee422125
LEE MINJI <m04j00@gmail.com>
2021-07-30 00:45:03 +0900

    카카오맵 API 연동

--- app/src/main/java/com/example/pregoing/FragmentHome.java
+++ app/src/main/java/com/example/pregoing/FragmentHome.java
@@ -9,10 +9,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
 public class FragmentHome extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        MapView mapView = new MapView(getActivity());
+        ViewGroup mapViewContainer = (ViewGroup) v.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        // 중심점 변경 + 줌 레벨 변경
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.55759443948931, 126.92446076240746), 1, true);
+
+        return v;
     }
 }
