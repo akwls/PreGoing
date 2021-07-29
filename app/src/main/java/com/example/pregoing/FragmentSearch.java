ac214200bfe5cef27f10680fd83626293052d7ac
LEE MINJI <m04j00@gmail.com>
2021-07-30 06:19:56 +0900

    특정 장소 선택 시 장소 선택 화면창으로 이동 구현

--- app/src/main/java/com/example/pregoing/FragmentSearch.java
+++ app/src/main/java/com/example/pregoing/FragmentSearch.java
@@ -1,18 +1,34 @@
 package com.example.pregoing;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentSearch extends Fragment {
+
+    public static final int sub = 1001;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View v =  inflater.inflate(R.layout.fragment_search, container, false);
+
+        Button button = (Button)v.findViewById(R.id.search_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),choosePage.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
