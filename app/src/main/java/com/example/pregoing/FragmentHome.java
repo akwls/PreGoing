790f59edbde55167bc8e24797cb1457ce5cc9fb7
LEE MINJI <m04j00@gmail.com>
2021-07-30 12:01:58 +0900

    방문 장소 추가 시 메인 화면에 장소 띄우기

--- app/src/main/java/com/example/pregoing/FragmentHome.java
+++ app/src/main/java/com/example/pregoing/FragmentHome.java
@@ -2,11 +2,14 @@ package com.example.pregoing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +22,7 @@ public class FragmentHome extends Fragment {
     public static FragmentHome newInstance() {
         return new FragmentHome();
     }
+    int cnt = 0;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,12 +36,25 @@ public class FragmentHome extends Fragment {
         mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.55759443948931, 126.92446076240746), 1, true);
 
         ImageButton searchBtn = (ImageButton) v.findViewById(R.id.search_btn);
+        ImageButton threeBtn = (ImageButton) v.findViewById(R.id.three_btn);
+        final LinearLayout linearLayout = (LinearLayout) v.findViewById(R.id.pre_place);
+        threeBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Log.d("myapp", "클릭");
+                linearLayout.setVisibility(View.VISIBLE);
+            }
+        });
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("myapp", "클릭");
                 ((MainActivity)getActivity()).replaceFragment(FragmentSearch.newInstance());
+
             }
+
         });
 
 
